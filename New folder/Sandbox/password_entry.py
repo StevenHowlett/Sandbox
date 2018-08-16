cd1ac0f51169aac54a32960ddcae77240b5acc1f
jc484989 <Bathboys1>
2018-08-16 13:42:11 +1000

    add list files program

--- New folder/Sandbox/password_entry.py
+++
@@ -1,6 +0,0 @@
-"""Steven Howlett"""
-MIN_LENGTH=3
-password=input("password:")
-while len(password) < MIN_LENGTH:
-    password = input("enter password with a min of 3 characters")
-print('*'*len(password))
